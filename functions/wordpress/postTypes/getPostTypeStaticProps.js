e22ad83aad6115f09897b01931566c3d50ba3855
Andrew Harris <aceharris@hotmail.co.uk>
2021-10-09 14:28:55 +0100

    speed up fetching

--- functions/wordpress/postTypes/getPostTypeStaticProps.js
+++ functions/wordpress/postTypes/getPostTypeStaticProps.js
@@ -26,7 +26,7 @@ export default async function getPostTypeStaticProps(
   previewData = null
 ) {
   // Set revalidate length (seconds).
-  const revalidate = 60 * 5
+  const revalidate = 10
 
   // Set sharedProps.
   const sharedProps = {
